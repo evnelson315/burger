d870622b8e9e36ac8b812a838b0a6fe1cc180a96
Evan Nelson <Evan Nelson.evnelson.315@gmail.com>
2016-07-30 14:45:40 -0500

    changes to connect

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 /*
 // Here is where you make the connection to the database and export and used by the O.R.M.
 // */
-// var mysql = require('mysql');
+var mysql = require('mysql');
 // var connection = mysql.createConnection({
 // 	port: 8889,
 // 	host: 'localhost',
@@ -19,7 +19,7 @@
 // });
 
 // module.exports = connection;
-
+//====================================================================
 
 var source = {
 
