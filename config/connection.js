63955b5bb752474ced09f864c1c038ffe4d0b8de
Evan Nelson <Evan Nelson.evnelson.315@gmail.com>
2016-07-30 14:13:50 -0500

    changes to connections.js

--- config/connection.js
+++ config/connection.js
@@ -19,3 +19,40 @@ connection.connect(function (err) {
 });
 
 module.exports = connection;
+
+
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'u8dd0j21ow03sod4',
+        password: "r8tq2xtmn7oovbrf",
+        database: "cat_db"
+    },
+    jawsDB: {
+         port: 3306,
+        host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'u8dd0j21ow03sod4',
+        password: "r8tq2xtmn7oovbrf",
+        database: "ajh8jslg3tfhfzbo"
+        // TODO: Add your JawsDB connection info here
+    }
+}
+
+
+// TODO: connect your jawsDB connection witht he mysql connection.
+var connection = mysql.createConnection(source.jawsDB);
+
+
+
+// Replace the connection code here with one to connect to you JawsDB connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
